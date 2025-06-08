48d122ce880748c1e190df470a20b9cf33366f98
pixel <pixelisprocrastinating15@gmail.com>
2025-06-08 19:29:21 +0200

    Update score.js

--- js/score.js
+++ js/score.js
@@ -11,10 +11,10 @@ const scale = 3;
  * @returns {Number}
  */
 export function score(rank, percent, minPercent) {
-    if (rank > 100) {
+    if (rank > 150) {
         return 0;
     }
-    if (rank > 100 && percent < 100) {
+    if (rank > 150 && percent < 100) {
         return 0;
     }
 
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-31.5*Math.pow(rank-1, 0.4) + 200) *
+    let score = (-33.5*Math.pow(rank-1, 0.4) + 250) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
