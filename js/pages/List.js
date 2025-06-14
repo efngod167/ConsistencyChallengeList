70ddce7e3c78fad08eec30244cf327ddfac0291f
pixel <pixelisprocrastinating15@gmail.com>
2025-06-14 09:29:02 +0200

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -193,20 +193,16 @@ export default {
               </li>
             </ol>
           </template>
-          <h3>Submission Requirements</h3>
+          <h3>List Requirements</h3>
           <p>Every level must be consistency based.</p>
           <p>Maximum CPS for your level must not be above 10.</p>
           <p>Have either source audio or clicks/taps in the video. Edited audio only does not count</p>
           <p>If your level contains inappropriate content (suggestive art, swastikas, slurs) it will not be added.</p>
           <p>You cannot include spam based parts (applies to the cps rule) in your level.</p>
-          <p>
-            You're allowed to use FPS Bypass but verifications/completions above 360 FPS or under 60 FPS will not be accepted. This rule is an exception for 2.2 users, as going above 240 FPS will result in the game being locked to 240 FPS.
-          </p>
-          <p>
-            CBF records are allowed for the list, however physics bypass is NOT allowed, but you can play in 2.1 to get up to 360 FPS.
-          </p>
-          <p>Alternating levels above 8 CPS will not be allowed on the list as a recent submission or record, exception being that you're allowed to alternate for only levels, such as "Diamond Blade End x20".
-          </p>
+          <p>You're allowed to use FPS Bypass but verifications/completions above 360 FPS or under 60 FPS will not be accepted. This rule is an exception for 2.2 users, as going above 240 FPS will result in the game being locked to 240 FPS.</p>
+          <p>CBF records are allowed for the list, however physics bypass is NOT allowed, but you can play in 2.1 to get up to 360 FPS.</p>
+          <p>Alternating levels will not be allowed on the list as a recent submission or record, exception being that you're allowed to alternate for only levels, such as "Diamond Blade End x20".</p>
+          <p>You are allowed to use Show Hitboxes on death, but your record will be denied if you use it for levels that are hard to see or invisible, such as "haruna challenge", "Psbpm v2", "Alec Challenge", "tims time" and "Drunk Punch".</p>
           <p>Once a level falls onto the Legacy List, we no longer accept records for them.</p>
         </div>
       </div>
