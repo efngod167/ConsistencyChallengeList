21450a0c6396974bcef4d7cb6e0f1e191d031b3a
pixel <pixelisprocrastinating15@gmail.com>
2025-11-01 08:32:32 +0100

    Update score.js

--- js/score.js
+++ js/score.js
@@ -24,7 +24,7 @@ export function score(rank, percent, minPercent) {
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
     */
     // New formula
-    let score = (-32.5*Math.pow(rank-1, 0.4) + 250) *
+    let score = (-23.468*Math.pow(rank-1, 0.4) + 200) *
         ((percent - (minPercent - 1)) / (100 - (minPercent - 1)));
 
     score = Math.max(0, score);
