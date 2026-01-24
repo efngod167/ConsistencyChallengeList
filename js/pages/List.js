a7b722e2b38584a77791c804724e69629872f76e
pixel <pixelisprocrastinating15@gmail.com>
2026-01-24 21:56:07 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -202,7 +202,7 @@ export default {
 		<p>You are allowed to make dupes of challenges, if it's an extension or a level repeated specific times in a row, whatever is considered a dupe. If the level has more than 2 dupes on the list, we would have to remove the easier level from it, depending on how low it has been placed at.</p>
 		    <h3>Submission Requirements</h3>
         <p>You're allowed to use FPS Bypass but verifications/completions above 360 FPS or under 60 FPS will not be accepted. This rule is an exception for 2.2 users, as going above 240 FPS will result in the game being locked to 240 FPS.</p>
-        <p>CBF/CBM records are allowed for the list, however Physics Bypass is NOT allowed, but you can play in 2.1 to get up to 360 FPS.</p>
+        <p>CBF/CBM records are allowed for the list, however Physics Bypass is NOT allowed, but you can play in 2.1 to get up to 360 FPS. If you don't know how to downgrade versions, you'll be provided with <a style="color: #03bafc" href="https://www.youtube.com/watch?v=JMBn04BWTJc">this tutorial</a>. If you have any issues, contact staff in the server.</p>
         <p>You're only allowed to use Physics Bypass (or Frame Extrapolation if on Mega Hack v9) when the level is FPS Locked (for ex. Binbo X) and can be played on either both versions or only 2.2. This does not mean fixes for 2.1 FPS Locked levels are allowed for 2.2.</p>
         <p>Cube Challenges are not allowed to be beaten with CBF, but with the FPS required for the level.</p>
 		<p>There must be a cheat indicator with clicks/taps in your completion/submission, however you are not allowed to use "Click Sounds" or "Click Sounds Lite" in Geode as they're click sounds, not real clicks.</p>
