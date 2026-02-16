9e29b31d2106ba4c4430ab99f65357dc10b164cc
pixel <pixelisprocrastinating15@gmail.com>
2026-02-16 18:13:03 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -194,7 +194,7 @@ export default {
             </ol>
           </template>
         <h3>Level Requirements</h3>
-        <p>Every level must be consistency based.</p>
+        <p>Your level must be **consistency based**.</p>
         <p>The maximum CPS for your level cannot exceed 9.5 or higher.</p>
 		<p>Your level must be under 30 seconds.</p>
 		<p>If your level contains inappropriate content (suggestive art, swastikas, slurs) it will not be added.</p>
