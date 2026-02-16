10efa9e153c10cda83c967968cf7a8d9cd496bef
pixel <pixelisprocrastinating15@gmail.com>
2026-02-16 18:34:37 +0100

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -194,7 +194,7 @@ export default {
             </ol>
           </template>
         <h3>Level Requirements</h3>
-        <p>Your level must be _consistency based_.</p>
+        <h1>Your level <p>must be consistency based.</p> A good example of a Consistency Challenge is DIOOZ by JoinMolten, and a bad example is true binjus chal by uwoGD.</h1>
         <p>The maximum CPS for your level cannot exceed 9.5 or higher.</p>
 		<p>Your level must be under 30 seconds.</p>
 		<p>If your level contains inappropriate content (suggestive art, swastikas, slurs) it will not be added.</p>
