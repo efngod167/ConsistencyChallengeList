e20280e099457c859f5c8424c285504f951ba9b4
sadisyourlife1 <goga.mormin2@list.ru>
2026-03-31 21:55:12 +0300

    Update util.js

--- js/util.js
+++ js/util.js
@@ -10,7 +10,7 @@ export function embed(video) {
 }
 
 export function localize(num) {
-    return num.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 0 });
+    return num.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 1 });
 }
 
 export function getThumbnailFromId(id) {
