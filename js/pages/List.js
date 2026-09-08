1cf97921b6e0a00a302c3b2ec4ba9202f33e173f
blighted <216200173+seaned1@users.noreply.github.com>
2026-09-08 21:21:41 +0400

    Update List.js

--- js/pages/List.js
+++ js/pages/List.js
@@ -194,7 +194,7 @@ export default {
             </ol>
           </template>
         <h4>Level Requirements</h4>
-        <p>Your level <b>must be consistency based.</b> A good example of a consistency challenge is <b>DIOOZ</b> by JoinMolten, and a bad example is <b>swi consistency II</b> by uwoGD. Challenges that repeat a specific part of the level aren't allowed, as they aren't really considered a consistency challenge.</p>
+        <p>Your level <b>must be consistency based.</b> A good example of a consistency challenge is <b>DIOOZ</b> by JoinMolten, and a bad example is <b>swi consistency II</b> by SeCeP. Challenges that repeat a specific part of the level aren't allowed, as they aren't really considered a consistency challenge.</p>
 		<p>There needs to be some effort put into your levels, as layouts aren't really allowed.</p>
 		<p>The minimum amount of clicks or releases your challenge can have is 4. Anything below that <b>doesn't qualify</b> as a consistency challenge.</p>
         <p>The maximum CPS for your level <b>cannot</b> exceed 9.5 or higher.</p>
